3797914f2a3c8a88daa279958d29ef9ecde09af5
saqlain021 <saqlainali.saqlainali08@gmail.com>
2024-05-25 17:13:24 +0500

    number guessing from 1 to 100.

---
+++ main.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node 
+import inquirer from "inquirer";
+let number=Math.floor(Math.random()*100+1);
+
+let score=100
+
+for(let chances=99;chances>=0;chances--){
+    let usr=await inquirer.prompt([{
+        name:"input",
+        message:"enter your guess number here:",
+        type:"number"
+    }]);
+    if(usr.input===number){
+        console.log("\nyour guess is right.");
+        break
+    }
+    else if(usr.input<number){
+        console.log("\nyour guess is lower, try higher number");
+    }
+    else if(usr.input>number){
+        console.log("\nyou guess is higher, try lower number")
+    }else{
+        console.log("\nNot a number")
+    }
+    console.log(`your remaining chances are:${chances}\n`)
+    
+
+  
+  score--
+
+}
+console.log(`your score is :${score}`)
+console.log(`the right number is:${number}`)
